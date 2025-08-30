a1dd5b2b31e501a74c0a34697def03d318886e4d
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-08-30 23:57:48 +0530

    first commit

--- src/main/java/com/thirdeye3/telegrambot/utils/Initiatier.java
+++ src/main/java/com/thirdeye3/telegrambot/utils/Initiatier.java
@@ -5,16 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.thirdeye3.telegrambot.services.PropertyService;
+
 import jakarta.annotation.PostConstruct;
 
 @Component
 public class Initiatier {
+	
+	@Autowired
+	PropertyService propertyService;
 	
     private static final Logger logger = LoggerFactory.getLogger(Initiatier.class);
 	
 	@PostConstruct
     public void init() throws Exception{
         logger.info("Initializing Initiatier...");
+        propertyService.fetchProperties();
         logger.info("Initiatier initialized.");
     }
 
